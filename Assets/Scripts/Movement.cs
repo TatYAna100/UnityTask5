8c6c5a42d0656c15170ba16d013ee3a2c60fb2ba
TatYAna100 <102324751+TatYAna100@users.noreply.github.com>
2022-09-05 18:53:23 +0300

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,12 +6,14 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+    private const string _idle = "Idle";
+    private const string _run = "Run";
     private Animator _animator;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
-        _animator.SetTrigger("Idle");
+        _animator.SetTrigger(_idle);
     }
 
     private void Update()
@@ -19,23 +21,23 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(_speed * Time.deltaTime, 0, 0);            
-            _animator.SetTrigger("Run");
+            _animator.SetTrigger(_run);
         }
         else if (Input.GetKeyUp(KeyCode.D))
         {
-            _animator.ResetTrigger("Run");
-            _animator.SetTrigger("Idle");
+            _animator.ResetTrigger(_run);
+            _animator.SetTrigger(_idle);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
-            _animator.SetTrigger("Run");
+            _animator.SetTrigger(_run);
         }
         else if (Input.GetKeyUp(KeyCode.A))
         {
-            _animator.ResetTrigger("Run");
-            _animator.SetTrigger("Idle");
+            _animator.ResetTrigger(_run);
+            _animator.SetTrigger(_idle);
         }
     }
 }
